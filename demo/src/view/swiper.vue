f1dfb4ffd0666ae9db7e959ff4b904af042a904e
yujiuchun <867042755@qq.com>
2019-06-12 20:05:04 +0800

    这是第一次上传

---
+++ demo/src/view/swiper.vue
@@ -0,0 +1,34 @@
+<template>
+  <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+    <!-- slides -->
+    <swiper-slide><img src="../assets/111.jpg" alt=""></swiper-slide>
+    <swiper-slide><img src="../assets/222.jpg" alt=""></swiper-slide>
+    <swiper-slide><img src="../assets/333.jpg" alt=""></swiper-slide>
+    <!-- <div class="swiper-pagination"  slot="pagination"></div>
+    <div class="swiper-button-prev" slot="button-prev"></div>
+    <div class="swiper-button-next" slot="button-next"></div>
+    <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
+  </swiper>
+</template>
+
+<script>
+import {swiper, swiperSlide} from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+export default {
+  name: 'HelloWorld',
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
